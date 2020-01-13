344d9b3ee9fffb3e127b48d571113d38d782105d
rasim321 <rasim.alam.ra@gmail.com>
2020-01-13 16:39:21 -0500

    colored and counted veggies

---
+++ veggies.py
@@ -0,0 +1,19 @@
+import csv
+
+vegetables = [
+ {"name": "eggplant", "color": "purple"},
+ {"name": "tomato", "color": "red"},
+ {"name": "corn", "color": "yellow"},
+ {"name": "okra", "color": "green"},
+ {"name": "arugula", "color": "green"},
+ {"name": "broccoli", "color": "green"},
+]
+
+with open('vegetables.csv', 'w') as f:
+	writer = csv.writer(f)
+	writer.writerow(['name', 'color'])
+
+	for i in vegetables:
+		writer.writerow([i["name"], i["color"], len(i["name"])])
+
+#worked with Inayat and Mia
